4a0680f5f6afe219ac7453ba854d6726ae10aae0
koalium <kh.industry.co@gmail.com>
2025-02-24 19:29:28 +0330

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,6 +2,6 @@ from cx_Freeze import setup, Executable
 
 setup(
 
-    name="rupturium",version="0.4",description="its rupture disk manufacturing helper",executables=[Executable("mgui.py")],
+    name="rupturium",version="0.5.3",description="its rupture disk manufacturing helper",executables=[Executable("main.py")],
 
 )   
